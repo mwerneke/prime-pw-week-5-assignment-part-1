2ab9f8275a5b45ca0cac7bfd3e6c26268fed6435
Michael Werneke <mike.werneke@gmail.com>
2021-07-06 12:51:55 -0500

    week5pt1

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -1,4 +1,4 @@
-console.log('***** Object Practice *****')
+console.log('***** Object Practice *****');
 
 // Add the required code to complete the tasks below
 
@@ -8,33 +8,47 @@ console.log('***** Object Practice *****')
   - Give it  a numeric 'shoeCount' property
   - Also give it an array 'favThreeFoods'
 */
-const me = {
-  // TODO - add properties here
-};
+let me = {
+  firstName:'Michael',
+  lastName:'Werneke',
+  hasSibling: true,
+  shoeCount: 8,
+  favThreeFoods: ['Steak', 'Fruit', 'Pizza']
+
+}// end me
+
 console.log('A little about me:', me);
 
 /* 2. Accessing object properties.
-  - Create a variable called fullName 
-  - Use the firstName & lastName properties of the object you 
-    created above to set its value. 
+  - Create a variable called fullName
+  - Use the firstName & lastName properties of the object you
+    created above to set its value.
   - Console.log fullName
 */
+let fullName = me.firstName + me.lastName
+  console.log(fullName);
 
 
 
 /* 3. Nested arrays
   - Console.log your first favorite food
-  - Console.log your last favorite food 
+  - Console.log your last favorite food
 */
-
+  console.log('My first Favorite Food', me.favThreeFoods [ 0 ]);
+  console.log('My last Favorite Food', me.favThreeFoods [ 2 ]);
 
 
 /* 4. Change a property of an existing object.
   - Console.log your current shoe count.
   - Guess what?! You just got a new pair of shoes!!!
   - Set your object's shoeCount to be what it was plus 1
-  - Console.log your updated shoe count. 
+  - Console.log your updated shoe count.
 */
+  console.log(me.shoeCount);
+  me.shoeCount++
+    console.log('Guess what?!, I just got a new pair of shoes:', me.shoeCount);
+    console.log('Updated shoe count:', me.shoeCount);
+
 
 
 /* 5. Add a new property to an existing object.
@@ -43,3 +57,5 @@ console.log('A little about me:', me);
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
+  me.favoriteColor = "Blue";
+    console.log( me );
